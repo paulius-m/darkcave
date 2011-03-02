bf41433541433fd4b340009659fa0b2f96272bed
Paulius Morkunas <guns.dream@gmail.com>
2011-03-02 22:59:37 +0200

    start point

---
+++ darkcave/darkcave/Camera.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace darkcave
+{
+    class Camera
+    {
+        public float AspectRatio;
+        public Matrix Projection;
+        public Matrix View;
+
+
+
+        public Camera()
+        {
+            GraphicsDeviceManager graphics = Game1.Instance.graphics;
+            AspectRatio = graphics.GraphicsDevice.Viewport.Width * 1.0f / graphics.GraphicsDevice.Viewport.Height;
+
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), AspectRatio, 1.0f, 1000.0f);
+
+            View = Matrix.CreateLookAt(new Vector3(15, 15, 60), new Vector3(15, 15, 0), Vector3.Up);
+        }
+
+        public void Update()
+        {
+
+        }
+
+
+    }
+}
---
+++ darkcave/darkcave/Map.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace darkcave
+{
+    class Map
+    {
+        protected class Node
+        {
+            public bool Filled;
+        }
+
+        public Model model;
+        public Vector3 Size;
+
+        protected Node[,] Data;
+
+        public Map()
+        { 
+        
+        }
+
+        public void Init()
+        {
+            Data = new Node[(int)Size.X, (int)Size.Y];
+            Random r = new Random(0);
+
+            for (int i1 = 0; i1 < Size.X; i1++)
+            for (int i2= 0; i2< Size.Y; i2++)
+            {
+                Data[i1, i2] = new Node { Filled = (r.Next(i2/2) == 0) };
+            }
+        }
+
+        public void Draw(Camera cam)
+        {
+            
+            for (int m = 0; m < model.Meshes.Count; m++)
+            {
+                ModelMesh mesh = model.Meshes[m];
+
+                foreach (BasicEffect e in mesh.Effects)
+                {
+                    e.View = cam.View;
+                    e.Projection = cam.Projection;
+                    e.DiffuseColor = new Vector3();
+                    for (int i1 = 0; i1 < Size.X; i1++)
+                    for (int i2 = 0; i2 < Size.Y; i2++)
+                    {
+
+                        if (Data[i1, i2].Filled == false)
+                            continue;
+
+                        Matrix world = Matrix.CreateTranslation(new Vector3(i1, i2, 0));
+
+                        e.World = world;
+                        mesh.Draw();
+                    }
+                }
+            }
+        }
+
+    }
+}
